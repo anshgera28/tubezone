0d3022216b771ad285e4d603eb5f5af80ebf4375
Ansh Gera <anshgera947@gmail.com>
2025-06-10 22:23:38 +0530

    fixing bugs in app js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.error("Error connecting to DB : ", error);
+    process.exit(1);
+})
 
 
 
